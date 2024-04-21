e396ba924529aa88ee2a587a32a332b457b2ac1a
useon <useon@users.noreply.github.com>
2024-04-21 09:00:23 +0000

    deploy: b46a9cfd5e5e1b5ac1c146ccfebca5eab9f88d14

---
+++ storybook/assets/Input-Cju5DitC.js
@@ -0,0 +1,12 @@
+import{j as o}from"./jsx-runtime-BnIj46N_.js";import{u as t}from"./styled-components.browser.esm-BDDUX98t.js";const n=t.input`
+  width: 100%;
+  height: 16px;
+  padding: 8px;
+  border-radius: 4px;
+  outline: none;
+  border: 1px solid #acacac;
+
+  ${r=>r.isError&&`
+    border-color: red;
+  `}
+`,s=({isError:r,...e})=>o.jsx(o.Fragment,{children:o.jsx(n,{isError:r,...e})});s.__docgenInfo={description:"",methods:[],displayName:"Input",props:{isError:{required:!1,tsType:{name:"boolean"},description:""}}};export{s as I};
